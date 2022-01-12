68e5e4e80cde83750e0a4dd888070ad1fe1d85a1
FranQUI <fquivron@gmail.com>
2022-01-12 09:19:11 +0100

    Dont show bokking link if competition is over

--- server.py
+++ server.py
@@ -35,10 +35,6 @@ competitions = load_competitions()
 clubs = load_clubs()
 
 
-
-
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from bs4 import BeautifulSoup as bs
 
 import pytest
 import server
@@ -12,12 +13,26 @@ class TestClient:
             {
                 "name": "Spring Festival",
                 "date": "2020-03-27 10:00:00",
-                "numberOfPlaces": "25"
+                "numberOfPlaces": "25",
+                "over": True
             },
             {
                 "name": "Fall Classic",
                 "date": "2020-10-22 13:30:00",
-                "numberOfPlaces": "13"
+                "numberOfPlaces": "13",
+                "over": True
+            },
+            {
+                "name": "Future Competition",
+                "date": "3000-10-22 13:30:00",
+                "numberOfPlaces": "18",
+                "over": False
+            },
+            {
+                "name": "Over Competition",
+                "date": "3000-10-22 13:30:00",
+                "numberOfPlaces": "18",
+                "over": True
             }
         ]
         server.clubs = [
@@ -39,26 +54,35 @@ class TestClient:
 
 class TestIndex(TestClient):
     def test_index_should_have_response_code_200(self, test_client):
-        res = test_client.get("/")
-        assert res.status_code == 200
+        response = test_client.get("/")
+        assert response.status_code == 200
 
     def test_index_use_index_template(self, test_client):
-        res = test_client.get("/")
-        assert str(res.data).find("Welcome to the GUDLFT Registration Portal!") > 0
+        response = test_client.get("/")
+        assert str(response.data).find("Welcome to the GUDLFT Registration Portal!") > 0
 
 
 class TestShowSummary(TestClient):
     def test_should_have_response_code_200(self, test_client):
-        res = test_client.post("/showSummary", data={'email': 'good@email.com'})
-        assert res.status_code == 200
+        response = test_client.post("/showSummary", data={'email': 'good@email.com'})
+        assert response.status_code == 200
 
     def test_bad_email_should_have_response_code_301(self, test_client):
-        res = test_client.post("/showSummary", data={'email': 'bad@email.com'})
-        assert res.status_code == 301
+        response = test_client.post("/showSummary", data={'email': 'bad@email.com'})
+        assert response.status_code == 301
 
     def test_no_email_should_have_response_code_301(self, test_client):
-        res = test_client.post("/showSummary", data={'email': ''})
-        assert res.status_code == 301
+        response = test_client.post("/showSummary", data={'email': ''})
+        assert response.status_code == 301
 
+    def test_competition_show_book_link_if_not_over(self, test_client):
+        competition = "FutureCompetition"  # CSS selector is competition name without spaces
+        response = test_client.post("/showSummary", data={'email': 'good@email.com'})
+        response_content = bs(response.data, 'html.parser')
+        assert str(response_content.select(f"#{competition}")).find("<a class=\"book_link\"")
 
-
+    def test_competition_not_show_book_link_if_over(self, test_client):
+        competition = "OverCompetition"  # CSS selector is competition name without spaces
+        response = test_client.post("/showSummary", data={'email': 'good@email.com'})
+        response_content = bs(response.data, 'html.parser')
+        assert str(response_content.select(f"#{competition}")).find("<a class=\"book_link\"") < 0
